748dbe332b6f368e2ae25244c6924d2cc1cd496d
Tushar-mahour <100329676+Tushar-mahour@users.noreply.github.com>
2024-06-02 21:23:19 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,43 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.29.0"
+    }
+  }
+}
+
+provider "aws" {
+  access_key = "AKIAW3MEBMJTESMHX2OM"
+  secret_key = "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"
+  region     = "ap-south-1"
+}
+
+data "aws_vpc" "default_vpc" {
+  id = "vpc-0f624e4ecc843f288"
+}
+
+data "aws_security_group" "default_NSG" {
+  id = "sg-0d3f74d66dbeccb82"
+}
+
+data "aws_subnet" "default_subnet" {
+  id = "subnet-02da473617bab84e0"
+}
+
+resource "aws_instance" "TestVM" {
+  ami           = "ami-0287a05f0ef0e9d9a" # us-west-2
+  instance_type = "t2.micro"
+  subnet_id     = data.aws_subnet.default_subnet.id
+  count         = 1
+  key_name      = aws_key_pair.ssh_key.key_name
+  tags = {
+    Name = var.servername
+  }
+}
+
+resource "aws_key_pair" "ssh_key" {
+  key_name   = "tf-key-pair"
+  public_key = tls_private_key.vmkey.public_key_openssh
+}
+
