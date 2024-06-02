4dc333a38875320f28afc882f0885f0812b1b64d
Tushar-mahour <100329676+Tushar-mahour@users.noreply.github.com>
2024-06-02 21:23:53 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,32 @@
+variable "servername" {
+  description = "Name of the server"
+  #default     = " "
+}
+
+# variable "subnet" {
+#   description = "Existing subnet"
+# }
+
+# variable "sg" {
+#   description = "security group name" 
+# }
+
+# variable "bot" {
+#   description = "Bot Name"  
+# }
+
+# variable "cicd_public_key" {
+#   description = "Public key of cicd prod jenkins"
+#   type        = string
+# }
+
+# variable "ansible_public_key" {
+#   description = "Public key of ansible"
+#   type        = string
+# }
+
+# variable "creator" {
+#   description = "Machine Creator"
+#   default     = "Terraform"
+# }
+
